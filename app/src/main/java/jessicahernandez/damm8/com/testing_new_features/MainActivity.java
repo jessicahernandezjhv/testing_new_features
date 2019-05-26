cc2de46b255f5989d1defd6cdd7e15f7e406adc4
Jessica Hernandez <cf17jessica.hernandez@iesjoandaustira.org>
2019-05-26 12:16:59 +0200

    Navigation between fragments working

--- app/src/main/java/jessicahernandez/damm8/com/testing_new_features/MainActivity.java
+++ app/src/main/java/jessicahernandez/damm8/com/testing_new_features/MainActivity.java
@@ -1,13 +1,63 @@
 package jessicahernandez.damm8.com.testing_new_features;
 
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentPlatos.OnFragmentInteractionListener, FragmentHacerReserva.OnFragmentInteractionListener, FragmentVerReservas.OnFragmentInteractionListener {
+    Toolbar toolbar;
+    FragmentPlatos platosMenu;
+    FragmentHacerReserva hacerReserva;
+    FragmentVerReservas verReservas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        platosMenu = new FragmentPlatos();
+        hacerReserva = new FragmentHacerReserva();
+        verReservas = new FragmentVerReservas();
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        switch (item.getItemId()){
+            case R.id.menuPlatosID:
+                transaction.replace(R.id.contenedorFragments, platosMenu);
+                break;
+            case R.id.menuHacerReservaID:
+                transaction.replace(R.id.contenedorFragments, hacerReserva);
+                break;
+            case R.id.menuVerReservaID:
+                transaction.replace(R.id.contenedorFragments, verReservas);
+                break;
+        }
+        transaction.commit();
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
