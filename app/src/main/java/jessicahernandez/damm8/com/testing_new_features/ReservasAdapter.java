018f9eeca7b5adbc5c3835fa11d0aa760d368ff9
Jessica Hernandez <cf17jessica.hernandez@iesjoandaustria.org>
2019-05-26 21:54:45 +0200

    Recycler adapter created

--- app/src/main/java/jessicahernandez/damm8/com/testing_new_features/ReservasAdapter.java
+++ app/src/main/java/jessicahernandez/damm8/com/testing_new_features/ReservasAdapter.java
@@ -2,6 +2,7 @@ package jessicahernandez.damm8.com.testing_new_features;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
 
 public class ReservasAdapter extends RecyclerView.Adapter<ReservasAdapter.RecyclerViewHolder> {
@@ -20,5 +21,12 @@ public class ReservasAdapter extends RecyclerView.Adapter<ReservasAdapter.Recycl
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+        public RecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 }
