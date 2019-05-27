90ffb6a3f8d27e23f0fa7ef6ee39543bd2dcb56c
Jessica Hernandez <cf17jessica.hernandez@iesjoandaustria.org>
2019-05-27 15:47:07 +0200

    Loading data from firebase database in recyclerview with loading spinner

--- app/src/main/java/jessicahernandez/damm8/com/testing_new_features/FragmentVerReservas.java
+++ app/src/main/java/jessicahernandez/damm8/com/testing_new_features/FragmentVerReservas.java
@@ -3,13 +3,24 @@ package jessicahernandez.damm8.com.testing_new_features;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+
+import static android.view.View.VISIBLE;
 
 
 /**
@@ -30,8 +41,11 @@ public class FragmentVerReservas extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    ProgressBar spinner;
     RecyclerView recyclerView;
     ArrayList<HacerReservaModel> listaReservas;
+    FirebaseDatabase firebaseDatabase;
+    ReservasAdapter adapter;
 
     private OnFragmentInteractionListener mListener;
 
@@ -67,10 +81,52 @@ public class FragmentVerReservas extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_ver_reservas, container, false);
+
+        listaReservas = new ArrayList<>();
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerID);
+        spinner = (ProgressBar) view.findViewById(R.id.progressBar1);
+        spinner.setVisibility(View.VISIBLE);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new ReservasAdapter(listaReservas);
+        recyclerView.setAdapter(adapter);
+
+        //Obtener los datos de Realtime Database
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.getReference().child("reservas").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                HacerReservaModel reserva;
+                reserva = dataSnapshot.getValue(HacerReservaModel.class);
+                listaReservas.add(reserva);
+                adapter.notifyDataSetChanged();
+                spinner.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ver_reservas, container, false);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/jessicahernandez/damm8/com/testing_new_features/ReservasAdapter.java
+++ app/src/main/java/jessicahernandez/damm8/com/testing_new_features/ReservasAdapter.java
@@ -1,32 +1,70 @@
 package jessicahernandez.damm8.com.testing_new_features;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class ReservasAdapter extends RecyclerView.Adapter<ReservasAdapter.RecyclerViewHolder> {
+import java.util.ArrayList;
+
+public class ReservasAdapter extends RecyclerView.Adapter<ReservasAdapter.ReservasViewHolder> {
+    // Declaramos una lista para que almacene los datos de firebase y un intent
+    ArrayList<HacerReservaModel> listaReservas;
+    Intent intent;
+
+    public ReservasAdapter(ArrayList<HacerReservaModel> listaReservas) {
+        this.listaReservas = listaReservas;
+    }
 
     @NonNull
     @Override
-    public ReservasAdapter.RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public ReservasViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_item, null, false);
+        return  new ReservasViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ReservasAdapter.RecyclerViewHolder recyclerViewHolder, int i) {
-
+    public void onBindViewHolder(@NonNull ReservasViewHolder reservasViewHolder, int position) {
+        reservasViewHolder.verFecha.setText("Fecha: "+listaReservas.get(position).getFecha());
+        reservasViewHolder.verComensales.setText("Personas: "+listaReservas.get(position).getComensales());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaReservas.size();
     }
 
 
-    public class RecyclerViewHolder extends RecyclerView.ViewHolder {
-        public RecyclerViewHolder(@NonNull View itemView) {
+    public class ReservasViewHolder extends RecyclerView.ViewHolder {
+        TextView verFecha;
+        TextView verComensales;
+
+        public ReservasViewHolder(@NonNull View itemView) {
             super(itemView);
+            verFecha = itemView.findViewById(R.id.verFechaID);
+            verComensales = itemView.findViewById(R.id.verComensalesID);
+
+            // Enviamos los datos a la Activity con la vista detalle
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAdapterPosition();
+
+                    intent = new Intent(view.getContext(), HacerReservaModel.class);
+                    intent.putExtra("fecha_reserva", listaReservas.get(position).getFecha());
+                    intent.putExtra("comensales_reserva", listaReservas.get(position).getComensales());
+                    view.getContext().startActivity(intent);
+                }
+            });
+
+
+        }
+
+        public void asignarDatos(HacerReservaModel hacerReservaModel) {
+
         }
     }
 }
